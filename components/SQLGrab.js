842f572e7099e26e352e734d1a484bfb913c9352
hpenav <domotica.humberto@gmail.com>
2018-07-17 10:59:04 -0500

    push

--- components/SQLGrab.js
+++ components/SQLGrab.js
@@ -1,4 +1,12 @@
 var noflo = require('noflo');
+const sql = require('mssql')
+
+var config = {
+    user: 'octa',
+    password: 'octa204!',
+    server: '192.168.100.107', 
+    database: 'Veritrax5' 
+};
 
 exports.getComponent = function() {
   var c = new noflo.Component();
@@ -16,7 +24,24 @@ exports.getComponent = function() {
     if (!input.hasData('in')) {
       return;
     }
-    console.log("hey 4...");
+    // connect to your database
+    sql.connect(config, function (err) {
+
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+
+        // query to the database and get the records
+        request.query('SELECT * FROM Veritrax5.dbo.tblEvents', function (err, cursor) {
+
+            if (err) console.log(err)
+
+            var totalSQLRecords = (cursor.recordsets[0].length);
+            console.log("totalRecords: " + totalSQLRecords);
+
+        });
+    });
     // Read packets we need to process
     var data = input.getData('in');
     // Process data and send output
